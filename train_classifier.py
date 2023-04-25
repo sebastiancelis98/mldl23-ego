5ab9936f684dd08cc944b0e35be558232301698a
Sebastian Celis <celis.sebastian@yahoo.com>
2023-04-26 00:45:10 +0200

    Add missing initialization arguments and pip package to requirements

--- train_classifier.py
+++ train_classifier.py
@@ -56,7 +56,7 @@ def main():
         logger.info('{} Net\tModality: {}'.format(args.models[m].model, m))
         # notice that here, the first parameter passed is the input dimension
         # In our case it represents the feature dimensionality which is equivalent to 1024 for I3D
-        models[m] = getattr(model_list, args.models[m].model)()
+        models[m] = getattr(model_list, args.models[m].model)(num_classes, m, args.models[m], **args.models[m].kwargs)
 
     # the models are wrapped into the ActionRecognition task which manages all the training steps
     action_classifier = tasks.ActionRecognition("action-classifier", models, args.batch_size,
